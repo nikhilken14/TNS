bac607bd74c04fa3e1ac28904203f73d37c3cf05
nikhil <nikhilkenjale1314@gmail.com>
2025-01-30 16:03:59 +0530

    this decision program

---
+++ TNS/src/Day4/DecisionStatement.java
@@ -0,0 +1,13 @@
+package Day4;
+
+public class DecisionStatement {
+	public static void main(String[] args) {
+		int x=9;
+		if(x==5) {
+			System.out.println(" value of x is 5");
+		}
+		else {
+			System.out.println(" value of x is "+x);
+		}
+	}
+}
