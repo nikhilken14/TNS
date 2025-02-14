9ad42ede897a8d8dd72cabf1b69279df7394c01f
nikhil <nikhilkenjale1314@gmail.com>
2025-02-14 15:08:05 +0530

    this about static keyword

---
+++ TNS/src/Day9/StaticDemo.java
@@ -0,0 +1,31 @@
+package Day9;
+
+public class StaticDemo {
+	public static void main(String[] args) {
+		Empl e=new Empl("nikhil");
+		System.out.println(e);
+	}
+}
+
+class Empl{
+	private String Name;
+	static String com="TNS";
+	
+	public Empl(String Name) {
+		this.Name=Name;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Empl [Name : " + Name + " , Company : "+com+"]";
+	}
+	
+}
