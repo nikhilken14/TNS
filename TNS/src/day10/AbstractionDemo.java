9da60a67407071ccbc47a15466a0e652925aa7d9
nikhil <nikhilkenjale1314@gmail.com>
2025-02-18 11:56:32 +0530

    this about abstraction

---
+++ TNS/src/day10/AbstractionDemo.java
@@ -0,0 +1,54 @@
+package day10;
+
+public class AbstractionDemo {
+	public static void main(String[] args) {
+		Shape s = new Rectangle();
+		s.cal();
+		
+		Shape s1=new Square();
+		s1.cal();
+	}
+}
+
+abstract class Shape{
+	
+	public abstract void cal();
+	
+	public void show() {
+		System.out.println("this is concrete method of abstract class");
+	}
+}
+
+class Rectangle extends Shape{
+	float width;
+	float height;
+	
+	public Rectangle() {
+		width=3.2f;
+		height=2.0f;
+	}
+	
+	public Rectangle(float width,float height) {
+		this.width=width;
+		this.height=height;
+	}
+	
+	@Override
+	public void cal() {
+		System.out.println(width*height);
+	}
+	
+}
+
+class Square extends Shape{
+	float width;
+	
+	public Square() {
+		width=3.2f;
+	}
+	@Override
+	public void cal() {
+		System.out.println(width*width);
+	}
+	
+}
